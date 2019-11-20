752b31db049af0e678e9a19353fc81eb7eff047e
Cryptools <cryptoolstouch@gmail.com>
2019-11-20 11:22:13 +0300

    V 1.0.7

--- js/background.js
+++ js/background.js
@@ -1,3 +1,12 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.tabs.create({
+      url: 'https://cryptools.org/',
+      active: true
+    });
+  
+    return false;
+  });
+
 var coin = "BTC";
 
 var apiKey = "21bb9daeacff6d5a91b09bb3a2a8da791ce56f90029c46abb53290334d614e1c";
